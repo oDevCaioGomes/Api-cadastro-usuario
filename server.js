35e3c826debf130546aa51e0c5ad784c3f792521
CaioTompero <144059974+CaioTompero@users.noreply.github.com>
2025-04-14 11:20:42 -0300

    corrigindo porta pro Render

--- server.js
+++ server.js
@@ -105,7 +105,11 @@ app.delete('/usuarios/:id', async (req, res) => {
 
 })
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
+
 
 /*
     Criar nossa API de usuarios
